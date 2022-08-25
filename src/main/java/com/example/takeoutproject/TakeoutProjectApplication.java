97e103a22294c49e617ed58458f5e3f5a4395141
Chingpo-Lin <m13823601165@163.com>
2022-08-25 09:53:25 -0700

    first commit

---
+++ src/main/java/com/example/takeoutproject/TakeoutProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.takeoutproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeoutProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeoutProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/takeoutproject/TakeoutProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.takeoutproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeoutProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
